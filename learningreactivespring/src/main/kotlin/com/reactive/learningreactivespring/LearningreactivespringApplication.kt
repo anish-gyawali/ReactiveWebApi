7ffa445adea9695af09d53ba7c23a860caaabbcf
Anish Gyawali <anish.gyawali7@gmail.com>
2022-08-14 22:15:49 -0500

    initial setup

---
+++ learningreactivespring/src/main/kotlin/com/reactive/learningreactivespring/LearningreactivespringApplication.kt
@@ -0,0 +1,11 @@
+package com.reactive.learningreactivespring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningreactivespringApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearningreactivespringApplication>(*args)
+}
